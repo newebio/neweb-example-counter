2ae5fd412c9117e2db0b44e84d304e7ce41d90c4
Vitaly Arkhipov <arvitaly@list.ru>
2018-05-24 07:11:13 +0300

    new version of neweb

--- app/frames/index/controller.js
+++ app/frames/index/controller.js
@@ -3,7 +3,9 @@ const { BehaviorSubject, interval } = require("rxjs");
 const { map } = require("rxjs/operators");
 const controller = class {
     constructor() {
-        this.data$ = interval(1000).pipe(map(() => +new Date()));
+        this.data = {
+            tick: interval(1000).pipe(map(() => +new Date())),
+        };
     }
 }
 module.exports = {
--- app/frames/index/view.js
+++ app/frames/index/view.js
@@ -1,7 +1,13 @@
-const React = require("react");
-const view = class extends React.Component {
-    render() {
-        return React.createElement("div", {}, ["data::", this.props.data]);
+const Neweb = require("neweb");
+const { map } = require("rxjs/operators");
+const view = class extends Neweb.Component {
+    beforeInit() {
+        this.addElement("txt1", new Neweb.Text({
+            value: this.props.data.tick.pipe(map((v) => new Date(v).toString())),
+        }));
+    }
+    getTemplate() {
+        return `<div>Date::, <strong name="txt1"></strong></div>`;
     }
 }
 module.exports = {
