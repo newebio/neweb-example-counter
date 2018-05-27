8ed2b2b409e28b2f1b1c196811d9aa7615737664
Vitaly Arkhipov <arvitaly@list.ru>
2018-05-27 06:37:09 +0300

    up

--- app/frames/index/view.js
+++ app/frames/index/view.js
@@ -1,13 +1,13 @@
 const Neweb = require("neweb");
 const { map } = require("rxjs/operators");
 const view = class extends Neweb.Component {
-    beforeInit() {
-        this.addElement("txt1", new Neweb.Text({
+    beforeMount() {
+        this.addElement("txt1", new Neweb.TextNode({
             value: this.props.data.tick.pipe(map((v) => new Date(v).toString())),
         }));
     }
     getTemplate() {
-        return `<div>Date::, <strong name="txt1"></strong></div>`;
+        return `<div>Date::, <template name="txt1"></template></div>`;
     }
 }
 module.exports = {
